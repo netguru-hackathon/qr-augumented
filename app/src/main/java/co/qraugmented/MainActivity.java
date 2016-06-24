3052f246e20abc1995e8b7a9c6ffd7155849d636
bigapewhat <michael.karwowski@netguru.pl>
2016-06-24 16:36:20 +0200

    added AR overlay

--- app/src/main/java/co/qraugmented/MainActivity.java
+++ app/src/main/java/co/qraugmented/MainActivity.java
@@ -1,6 +1,9 @@
 package co.qraugmented;
 
 import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.PointF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +20,7 @@ public class MainActivity extends Activity implements QRCodeReaderView.OnQRCodeR
     private final static String TAG = MainActivity.class.getSimpleName();
     private ImageView mImageView;
     private QRCodeReaderView mydecoderview;
+    private TextView mPositionText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,7 @@ public class MainActivity extends Activity implements QRCodeReaderView.OnQRCodeR
 
         mydecoderview = (QRCodeReaderView) findViewById(R.id.qrdecoderview);
         mImageView = (ImageView) findViewById(R.id.augmentedImage);
+        mPositionText = (TextView) findViewById(R.id.postitionText);
         mydecoderview.setOnQRCodeReadListener(this);
 
     }
@@ -36,18 +41,27 @@ public class MainActivity extends Activity implements QRCodeReaderView.OnQRCodeR
     @Override
     public void onQRCodeRead(String text, PointF[] points) {
 
-        RelativeLayout.LayoutParams params1 = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        params1.leftMargin = getDip(points[0].x);
-        params1.topMargin = getDip(points[0].y);
-        params1.rightMargin = getDip(points[3].x);
-        params1.bottomMargin = getDip(points[3].y);
+        int width = mydecoderview.getWidth();
+        int height = mydecoderview.getHeight();
+
+        RelativeLayout.LayoutParams params1 = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+
+        params1.setMargins((int) (points[0].x - 100), (int) (points[1].y - 100), (int) (width - points[2].x - 100), (int) (height - points[2].y - 300));
+        mPositionText.setText(text);
+        if(text.toLowerCase().contains("netguru")) {
+            mImageView.setImageResource(R.drawable.netguru);
+        } else if (text.toLowerCase().contains("mcdonalds")) {
+            mImageView.setImageResource(R.drawable.mcdonalds);
+        } else if (text.toLowerCase().contains("uefa")) {
+            mImageView.setImageResource(R.drawable.euro);
+        } else {
+            mImageView.setImageResource(R.drawable.smiley);
+        }
+
 
         mImageView.setVisibility(View.VISIBLE);
         mImageView.setLayoutParams(params1);
 
-        for(int i = 0; i < points.length; i++) {
-            Log.d(TAG, "  Point " + i + " =  " + points[i]);
-        }
     }
 
     private int getDip(float pixels) {
